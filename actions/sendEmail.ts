f9fb58f79cbbd2dddddfc8388d0882708398848a
thiago bueno garcia <thiagobuenogarcia1@gmail.com>
2024-06-03 21:43:27 -0400

    send fix

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -2,6 +2,8 @@
 
 import { getErrorMessage, validateString } from "@/lib/utils"
 import { Resend } from "resend"
+import ContactFormEmail from "@/email/contact_form"
+import React from "react"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -9,29 +11,37 @@ export const sendEmail = async (formData: FormData) => {
     const message = formData.get('message')
     const senderEmail = formData.get('senderEmail')
 
-    if (!validateString(senderEmail, 200)) {
+    if (!validateString(senderEmail, 500)) {
         return {
             error: "Invalid sender email"
         }
     }
 
-    if (!validateString(message, 500)) {
+    if (!validateString(message, 5000)) {
         return {
             error: "Invalid message"
         }
     }
 
+    let data;
     try {
         await resend.emails.send({
             from: 'Contact Form <onboarding@resend.dev>',
             to: 'thiagobuenogarcia1@gmail.com',
             subject: 'Message from contact form',
             reply_to: senderEmail as string,
-            text: message as string,
+            react: React.createElement(ContactFormEmail, {
+                message: message as string,
+                senderEmail: senderEmail as string
+            })
         })
     } catch (error: unknown) {
         return {
             error: getErrorMessage(error)
         }
+    }
+
+    return {
+        data
     }
 }
