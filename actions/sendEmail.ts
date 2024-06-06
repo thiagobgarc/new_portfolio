d4280c5c2cb8a50a33a193dd9c5146cbb14a04d4
thiago bueno garcia <thiagobuenogarcia1@gmail.com>
2024-06-05 20:14:24 -0400

    styling on senderEmail

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,7 @@
 "use server"
-
-import { error } from "console"
 import { Resend } from "resend"
 import { getErrorMessage, validateString } from "@/lib/utils"
+import React from "react"
 
 const resend = new Resend(process.env.RESEND_API_KEY2)
 
@@ -21,12 +20,14 @@ export const sendEmail = async (formData: FormData) => {
   }
 
   try{
-    resend.emails.send({
+   await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
       to: "thiagobuenogarcia1@gmail.com",
       subject: `New message from ${senderEmail}`,
       reply_to: senderEmail as string,
       text: message as string,
+      html: `<h3 class='text-gray-700 text-4xl'>New message from ${senderEmail}</h3>
+      <p class='text-gray-800 text-3xl'>${message}</p>`,
     })
   } catch (error: unknown) {
     return {
