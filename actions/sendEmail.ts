482847a9f69a80c886b0de7ef822984fede29a9b
thiago bueno garcia <thiagobuenogarcia1@gmail.com>
2024-06-05 20:24:22 -0400

    fixed serversided error

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend"
 import { getErrorMessage, validateString } from "@/lib/utils"
 import React from "react"
 
-const resend = new Resend(process.env.RESEND_API_KEY2);
+const resend = new Resend(process.env.RESEND_API_KEY2)
 
 export const sendEmail = async (formData: FormData) => {
   console.log("Running sendEmailHandler")
